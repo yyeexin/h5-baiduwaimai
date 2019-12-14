9e51c0505fc6e1a132018ff86da8839edaf77a3c
yyeexin <248480904@qq.com>
2019-12-14 15:29:26 +0800

    commit

--- src/index.js
+++ src/index.js
@@ -1,38 +1,35 @@
 import Swiper from 'swiper'
-import axios from 'axios'
-import './index.scss'
+import './css/index.scss'
 import './lib/zepto/zepto'
 import './lib/zepto/event'
 import './lib/zepto/touch'
 import './lib/zepto/fx'
 import './lib/zepto/fx_methods'
-let swiper = new Swiper('.swiper-container', {
-	direction: 'vertical'
+var swiper = new Swiper('.swiper-container', {
+	direction: 'vertical',
+	on: {
+		slideChange: function() {
+			$('.swiper-slide')
+				.eq(this.activeIndex)
+				.addClass('animate')
+				.siblings()
+				.removeClass('animate')
+		}
+	}
 })
 
-$('.loading').click(function() {
-	$(this).fadeOut()
-})
-$('.welcome').click(function() {
-	$(this).fadeOut()
+$('.loading').addClass('animate')
+$('.loading .step').on('animationend', function() {
+	$('.loading').fadeOut(100, function() {
+		$('.welcome').addClass('animate')
+	})
 })
 
-// axios.get('http://api.yyeexin.com/weChat/getAccessToken').then(({ data }) => {
-// 	console.log(data)
-// })
-
-// axios
-// 	.post('http://api.yyeexin.com/weChat/getJSSDKConfig', {
-// 		url: window.location.href
-// 	})
-// 	.then(({ data }) => {
-// 		const { appId, nonceStr, signature, timestamp } = data
-// 		wx.config({
-// 			debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-// 			appId, // 必填，公众号的唯一标识
-// 			timestamp, // 必填，生成签名的时间戳
-// 			nonceStr, // 必填，生成签名的随机串
-// 			signature, // 必填，签名
-// 			jsApiList: ['updateAppMessageShareData', 'updateTimelineShareData'] // 必填，需要使用的JS接口列表
-// 		})
-// 	})
+$('.welcome .rotate-btn-box').longTap(function() {
+	$('.bear-box').css('animation', 'bearDisappear 1s forwards')
+	setTimeout(() => {
+		$('.welcome').fadeOut(1000, function() {
+			$('.page1').addClass('animate')
+		})
+	}, 1000)
+})
